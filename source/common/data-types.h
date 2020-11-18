b45dd04b955c2c5869d67052febf8a842da8c1b3
FatemaFawzy <fatemafst2009@gmail.com>
2020-11-18 19:17:58 +0200

    Forgot to push the data-types.h

--- source/common/data-types.h
+++ source/common/data-types.h
@@ -2,7 +2,7 @@
 #define OUR_DATA_TYPES_H
 
 #include <glm/glm.hpp>
-#include <bitset> 
+#include <bitset>
 
 namespace famm {
     // Since we may want to store colors in bytes instead of floats for efficiency,
@@ -10,11 +10,13 @@ namespace famm {
     typedef glm::vec<4, glm::uint8, glm::defaultp> Color;
 
     // ECS Types
-    typedef  std::uint8_t componentType;
+    typedef  std::uint8_t ComponentType;        //components and entities are defined as unsigned ints (Ids)
     typedef  std::uint32_t Entity;
-    typedef  std::bitset<32> Signature;
+    typedef  std::bitset<MAX_COMPONENTS> Signature;     //the signature on an entity is a bit set having the size of all components (1 in bits corresponding to components that the entity has)
 
-
+    //constants
+    const Entity MAX_ENTITIES = 5000;
+    const ComponentType MAX_COMPONENTS = 32;
 
 }
 
