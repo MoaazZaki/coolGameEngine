0f64ee99d8082dfae0c6f64942642b0ab39a956b
MoaazZaki <moaazzaki1999@gmail.com>
2020-11-19 10:41:45 +0200

    ECS + State manager integration

--- source/common/data-types.h
+++ source/common/data-types.h
@@ -9,15 +9,20 @@ namespace famm {
     // we are creating our own 32-bit R8G8B8A8 Color data type with the default GLM precision
     typedef glm::vec<4, glm::uint8, glm::defaultp> Color;
 
+    
+
     // ECS Types
     typedef  std::uint8_t ComponentType;        //components and entities are defined as unsigned ints (Ids)
     typedef  std::uint32_t Entity;
+
+    
+    const Entity MAX_ENTITIES = 5000;           //constants
+    const ComponentType MAX_COMPONENTS = 32;
+
     typedef  std::bitset<MAX_COMPONENTS> Signature;     //the signature on an entity/system is a bit set having the size of all components 
                                                         //(1 in bits corresponding to components that the entity/system has)
 
-    //constants
-    const Entity MAX_ENTITIES = 5000;
-    const ComponentType MAX_COMPONENTS = 32;
+    
 
 }
 
